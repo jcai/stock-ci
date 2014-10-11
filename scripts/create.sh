c12d6e5ac956d6393c72527dbac9ea350f741d22
Jun Tsai <jcai@ganshane.com>
2014-10-11 20:43:35 +0800

    add file

--- scripts/create.sh
+++ scripts/create.sh
@@ -9,7 +9,7 @@ git clone https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.g
 wget https://github.com/ganshane/shakey/releases/download/shakey-project-1.6/shakey-server-1.6-bin.tar.gz
 tar xfvz shakey-server-1.6-bin.tar.gz
 cd shakey-server*
-export BUILD_ID=`date +%Y%m%d`
+export BUILD_ID=`date +%Y-%m-%d`
 echo $BUILD_ID
 JAVA_OPTIONS="-Xmx1g" bin/shakey-server shakey.server.internal.Stockanalyzer ../ganshane.github.io/_posts/${BUILD_ID}-strong-stock.markdown
 cd -
