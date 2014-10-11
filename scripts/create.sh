d5206c7976b9e9938548bb6b8ffc411139dfb078
Jun Tsai <jcai@ganshane.com>
2014-10-11 19:04:04 +0800

    尝试增加编译脚本

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,17 +1,25 @@
 mkdir target
+
 cd target
-wget https://github.com/ganshane/shakey/releases/download/shakey-project-1.6/shakey-server-1.6-bin.tar.gz
-tar xfvz shakey-server-1.6-bin.tar.gz
-
-cd shakey-server*
-export BUILD_ID=`date +%Y%m%d`
-JAVA_OPTIONS="-Xmx1g" bin/shakey-server shakey.server.internal.Stockanalyzer ../../_posts/${BUILD_ID}-strong-stock.markdown
-
-cd ../..
 git config --global user.email "jcai@ganshane.com"
 git config --global user.name "Jun Tsai"
+git clone https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.git
+
+#process file using server gz
+wget https://github.com/ganshane/shakey/releases/download/shakey-project-1.6/shakey-server-1.6-bin.tar.gz
+tar xfvz shakey-server-1.6-bin.tar.gz
+cd shakey-server*
+export BUILD_ID=`date +%Y%m%d`
+echo $BUILD_ID
+JAVA_OPTIONS="-Xmx1g" bin/shakey-server shakey.server.internal.Stockanalyzer ../ganshane.github.io/_posts/${BUILD_ID}-strong-stock.markdown
+cd -
+
+#push post file
+cd ganshane.github.io
+ls _posts/*
 
 git add _posts
-git commit -a -m 'add ${BUILD_ID} strong stock'
-git remote set-url origin https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.git
+git commit -a -m "add ${BUILD_ID} strong stock"
+#git remote set-url origin https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.git
 git push origin
+
