76a70b6d3b582f9db6a84a15ca78903b5a123752
Jun Tsai <jcai@ganshane.com>
2014-10-12 16:05:54 +0800

    升级到1.6.1

--- scripts/create.sh
+++ scripts/create.sh
@@ -6,12 +6,12 @@ git config --global user.name "Jun Tsai"
 git clone https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.git
 
 #process file using server gz
-wget https://github.com/ganshane/shakey/releases/download/shakey-project-1.6/shakey-server-1.6-bin.tar.gz
-tar xfvz shakey-server-1.6-bin.tar.gz
+wget https://github.com/ganshane/shakey/releases/download/shakey-project-1.6.1/shakey-server-1.6.1-bin.tar.gz
+tar xfvz *.gz
 cd shakey-server*
 export BUILD_ID=`date +%Y-%m-%d`
 echo $BUILD_ID
-JAVA_OPTIONS="-Xmx1g" bin/shakey-server shakey.server.internal.Stockanalyzer ../ganshane.github.io/_posts/${BUILD_ID}-strong-stock.markdown
+JAVA_OPTIONS="-Xmx1g" bin/shakey-server shakey.server.internal.StockAnalyzerApp ../ganshane.github.io/_posts
 cd -
 
 #push post file
@@ -19,7 +19,7 @@ cd ganshane.github.io
 ls _posts/*
 
 git add _posts
-git commit -a -m "add ${BUILD_ID} strong stock"
+git commit -a -m "upgrade ${BUILD_ID} data"
 #git remote set-url origin https://${CI_USER}:${CI_PASS}@github.com/ganshane/ganshane.github.io.git
 git push origin
 
